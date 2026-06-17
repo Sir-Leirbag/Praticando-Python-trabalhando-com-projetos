4095618166840e3d529f46216be86053e91c184c
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-17 01:12:24 -0300

    Refatorar funções de cálculo de gorjeta e total, corrigindo a ordem dos parâmetros

--- Calculando a gorjeta em um restaurante/main.py
+++ Calculando a gorjeta em um restaurante/main.py
@@ -1,16 +1,18 @@
+# 1. Funções
+def calcular_gorjeta(conta, porcentagem):
+    return conta * (porcentagem / 100)
+
+def calcular_total(conta, gorjeta):
+    return conta + gorjeta
+
+# 2. Entrada de dados
 conta = float(input('Digite o valor total da conta: R$ '))
 porcentagem = float(input('Digite a porcentagem de gorjeta: % '))
 
-def calcular_gorjeta (porcentagem, conta):
-    valor_da_gorjeta = conta * (porcentagem / 100)
-    return valor_da_gorjeta
-
-def calcular_total (conta, porcentagem):
-    total = conta + calcular_gorjeta(porcentagem, conta)
-    return total
-
-valor_da_gorjeta = calcular_gorjeta(porcentagem, conta)
+# 3. Processamento
+valor_da_gorjeta = calcular_gorjeta(conta, porcentagem)
 total = calcular_total(conta, porcentagem)
 
+#4. Saída
 print(f'\nValor da gorjeta: R$ {valor_da_gorjeta:.2f}')
 print(f'Total a pagar: R$ {total:.2f}')
