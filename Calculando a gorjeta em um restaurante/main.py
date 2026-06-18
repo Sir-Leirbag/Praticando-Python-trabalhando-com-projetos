f00b97f8a4b899874036855a7f0671869427dc20
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-17 23:09:15 -0300

    Adicionar tratamento de exceção para entrada de dados e reorganizar o fluxo de processamento

--- Calculando a gorjeta em um restaurante/main.py
+++ Calculando a gorjeta em um restaurante/main.py
@@ -5,14 +5,17 @@ def calcular_gorjeta(conta, porcentagem):
 def calcular_total(conta, gorjeta):
     return conta + gorjeta
 
-# 2. Entrada de dados
-conta = float(input('Digite o valor total da conta: R$ '))
-porcentagem = float(input('Digite a porcentagem de gorjeta: % '))
+try:
+    # 2. Entrada de dados
+    conta = float(input('Digite o valor total da conta: R$ '))
+    porcentagem = float(input('Digite a porcentagem de gorjeta: '))
 
-# 3. Processamento
-valor_da_gorjeta = calcular_gorjeta(conta, porcentagem)
-total = calcular_total(conta, valor_da_gorjeta)
+    # 3. Processamento
+    valor_da_gorjeta = calcular_gorjeta(conta, porcentagem)
+    total = calcular_total(conta, valor_da_gorjeta)
 
-# 4. Saída
-print(f'\nValor da gorjeta: R$ {valor_da_gorjeta:.2f}')
-print(f'Total a pagar: R$ {total:.2f}')
+    # 4. Saída
+    print(f'Valor da gorjeta: R$ {valor_da_gorjeta:.2f}')
+    print(f'Total a pagar: R$ {total:.2f}')
+except ValueError:
+    print('Digite apenas números.')
