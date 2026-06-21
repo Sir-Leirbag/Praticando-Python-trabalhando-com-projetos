bd8f38a6beccbe67ff9de8e4963fdcbcec05d315
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-21 15:04:12 -0300

    Remover parênteses desnecessários na mensagem de erro de CPF

--- Validando um CPF/main.py
+++ Validando um CPF/main.py
@@ -20,7 +20,7 @@ def segundo_digito(cpf):
 
 def valida_cpf(cpf):
     if not cpf.isdigit():
-        return('Erro: O CPF deve conter apenas números.')
+        return 'Erro: O CPF deve conter apenas números.'
     if len(cpf) != 11:
         return 'Erro: O CPF deve ter exatamente 11 dígitos.'
     if cpf == cpf[0] * 11:
