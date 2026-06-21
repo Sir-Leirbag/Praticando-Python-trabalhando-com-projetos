db91d8995ed7561307f71eaafd40bf69886ddec0
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-21 13:50:59 -0300

    Adicionar função para calcular o primeiro dígito do CPF

--- Validando um CPF/main.py
+++ Validando um CPF/main.py
@@ -1,3 +1,15 @@
+def primeiro_digito(cpf):
+    fatores = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+    # Separa os dígitos e multiplica cada um pelo seu fator correspondente
+    cpf_convertido = [int(digito) * fator for digito, fator in zip(cpf, fatores)]
+    soma = sum(cpf_convertido)
+    resto = soma % 11
+    if resto < 2:
+        return 0
+    else:
+        return 11 - resto
+        
+
 def valida_cpf(cpf):
     if not cpf.isdigit():
         return('Erro: O CPF deve conter apenas números.')
@@ -10,3 +22,4 @@ def valida_cpf(cpf):
 cpf = input('Digite seu CPF: ')
 
 print(valida_cpf(cpf))
+print(primeiro_digito(cpf))
