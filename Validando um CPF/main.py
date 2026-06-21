d7e01895baf131c248189c67959175865984ba03
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-21 03:41:51 -0300

    Corrigir verificação de CPF inválido para usar lista

--- Validando um CPF/main.py
+++ Validando um CPF/main.py
@@ -3,7 +3,7 @@ def valida_cpf(cpf):
         return('Erro: O CPF deve conter apenas números.')
     if len(cpf) != 11:
         return 'Erro: O CPF deve ter exatamente 11 dígitos.'
-    if cpf == 00000000000 or 12345678901 or 11111111111:
+    if cpf in ['00000000000', '12345678901', '11111111111']:
         return('Erro: CPF inválido.')
     return 'CPF válido.'
 
