17a13b2c16d3d01f21bc4bce91e39d5f119c30ea
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-21 03:37:17 -0300

    Adicionar verificação para CPFs inválidos na validação

--- Validando um CPF/main.py
+++ Validando um CPF/main.py
@@ -3,6 +3,8 @@ def valida_cpf(cpf):
         return('Erro: O CPF deve conter apenas números.')
     if len(cpf) != 11:
         return 'Erro: O CPF deve ter exatamente 11 dígitos.'
+    if cpf == 00000000000 or 12345678901 or 11111111111:
+        return('Erro: CPF inválido.')
     return 'CPF válido.'
 
 cpf = input('Digite seu CPF: ')
