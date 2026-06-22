6cdb0be01ab6d1dba0d1958e5db4dc6b8fa03e4f
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-22 00:30:52 -0300

    Corrigir formatação da impressão de palavras longas para usar a função join corretamente

--- Identificando palavras mais longas em um texto/main.py
+++ Identificando palavras mais longas em um texto/main.py
@@ -6,9 +6,9 @@ def seleciona_palavras(texto):
     return palavras_longas
 
 texto = input('Digite um texto: ')
-palavras_longas = (', '.join(seleciona_palavras(texto)))
+palavras_longas = seleciona_palavras(texto)
 
 if palavras_longas:
-    print(f'Palavras longas encontradas: {palavras_longas}')
+    print(f"Palavras longas encontradas: {', '.join(palavras_longas)}")
 else:
     print('Nenhuma palavra longa foi encontrada no texto.')
