b32a5447932d447dc44ce2b11e06e9d0a1aa173c
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-22 00:20:37 -0300

    Adicionar função para selecionar palavras longas em um texto

---
+++ Identificando palavras mais longas em um texto/main.py
@@ -0,0 +1,13 @@
+def seleciona_palavras(texto):
+    palavras_longas = []
+    for palavra in texto.split():
+        if len(palavra) >= 10:
+            palavras_longas.append(palavra)
+    return palavras_longas
+
+texto = input('Digite um texto: ')
+
+if seleciona_palavras(texto):
+    print(f'Palavras longas encontradas: {', '.join(seleciona_palavras(texto))}')
+else:
+    print('Nenhuma palavra longa foi encontrada no texto.')
