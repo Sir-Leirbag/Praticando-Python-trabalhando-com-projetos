c838db511ea5a665585782f463c1c45d4547086a
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-23 01:52:25 -0300

    Adicionar implementação do gerador de senhas seguras

---
+++ Gerador de senha segura/main.py
@@ -0,0 +1,28 @@
+import random
+
+def gerar_senha(senha):
+    for i in range(12):
+        senha.append((random.choice(elementos)))
+
+def validar_senha(senha):
+    if (any(elemento.islower() for elemento in senha)
+        and any(elemento.isupper() for elemento in senha)
+        and any(elemento.isdigit() for elemento in senha)
+        and any(not elemento.isalnum() for elemento in senha)
+        ):
+        return True
+    else:
+        return False
+
+def main(senha):
+    gerar_senha(senha)
+    if validar_senha(senha) == False:
+        senha = []
+        main(senha)
+    else:
+        print(f"Senha gerada: {''.join(senha)}")
+
+senha = []
+elementos = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?,.')
+
+main(senha)
