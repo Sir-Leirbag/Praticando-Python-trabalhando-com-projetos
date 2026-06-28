9e17ba3dc03e3f54beb289c7cb7401186da13622
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-06-28 13:29:19 -0300

    refatorar a lógica do jogo para remover a função main e simplificar a validação de entrada

--- Jogo de adivinhar o número/main.py
+++ Jogo de adivinhar o número/main.py
@@ -3,26 +3,22 @@ import random
 def gerar_numero_aleatorio():
     return random.randint(1,100)
 
-def comparar_numero(numero):
-    if numero < 1 or numero > 100:
-        print('Entrada inválida: Número fora do intervalo! Digite um número entre 1 e 100.')
-        main()
-    elif numero > numero_aleatorio:
-        print(f'Muito alto! Tente novamente: {numero}')
-        main()
-    elif numero < numero_aleatorio:
-        print(f'Muito baixo! Tente novamente: {numero}')
-        main()
-    else:
-        print(f'Parabéns! Você acertou o número: {numero}')
+numero_aleatorio = gerar_numero_aleatorio()
+tentativas = 0
 
-def main():
+while True:
     try:
         numero = int(input('Tente adivinhar o número (1-100): '))
     except ValueError:
         print('Entrada inválida: Digite apenas números.')
-        main()
-    comparar_numero(numero)
-
-numero_aleatorio = gerar_numero_aleatorio()
-main()
+        continue
+    tentativas += 1
+    if 1 > numero > 100:
+        print('Entrada inválida: Número fora do intervalo! Digite um número entre 1 e 100.')
+    elif numero > numero_aleatorio:
+        print(f'Muito alto! Tente novamente: {numero}')
+    elif numero < numero_aleatorio:
+        print(f'Muito baixo! Tente novamente: {numero}')
+    else:
+        print(f'Parabéns! Você acertou o número: {numero} com {tentativas} tentativas.')
+        break
